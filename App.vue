24c00a3c93d2df9fd78716153797d25fa3802f09
owner <owner1147@gmail.com>
2025-02-10 14:12:35 +0200

    add component

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <popup />
+  </div>
+</template>
+
+<script>
+import popup from "/popup/popup.vue";
+
+export default {
+  components: {
+    popup,
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  padding: 20px;
+}
+</style>
---
+++ popup/popup.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="app">
+    hello
+  </div>
+</template>
+<script></script>
+<style></style>
